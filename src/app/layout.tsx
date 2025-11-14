65b912a4f5f4932844f3ca9454a8ce310c842eea
AP Solanki <88495030+ateendra24@users.noreply.github.com>
2025-11-14 20:12:34 +0530

    Update font settings to use Satoshi throughout the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner"
 import { siteConfig } from "@/config/siteConfig";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -30,7 +19,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`font-Satoshi antialiased`}
         >
           <ThemeProvider
             attribute="class"
